1765359af2524860f4c2623e11efc7c025e1302c
Eric Lee <403739767@qq.com>
2019-05-07 00:49:59 +0800

    security和jwtFilter结合，实现servlet的过滤器url拦截

--- src/main/java/com/example/demo/controller/Index.java
+++ src/main/java/com/example/demo/controller/Index.java
@@ -1,12 +1,18 @@
 package com.example.demo.controller;
 
+import com.example.demo.auth.JwtUtil;
 import com.example.demo.rabbitMQ.AmqpComponent;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
 
 /**
  * Package : com.example.demo.controller
@@ -36,5 +42,41 @@ public class Index extends AuthController {
     @RequestMapping(value = "mqTest", method = RequestMethod.GET)
     public void mqTest() {
 
+    }
+
+    @ApiOperation(value = "", notes = "消息队列测试")
+    @PostMapping("/login")
+    public Object login(HttpServletResponse response,
+                        @RequestBody Account account) {
+        if (isValidPassword(account)) {
+            String jwt = JwtUtil.generateToken(account.username);
+            return new HashMap<String, String>() {{
+                put("token", jwt);
+            }};
+        } else {
+            return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+    @GetMapping("/api/admin")
+    @PreAuthorize("hasAuthority('ADMIN_USER')")
+    public @ResponseBody
+    Object helloToAdmin(String userId) {
+        return "Hello World! You are ADMIN ";
+    }
+
+    private boolean isValidPassword(Account ac) {
+        //we just have 2 hardcoded user
+        if ("admin".equals(ac.username) && "admin".equals(ac.password)
+                || "user".equals(ac.username) && "user".equals(ac.password)) {
+            return true;
+        }
+        return false;
+    }
+
+
+    public static class Account {
+        public String username;
+        public String password;
     }
 }
