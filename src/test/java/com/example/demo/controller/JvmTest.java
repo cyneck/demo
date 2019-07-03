fed415b4ccb0e397db57a51245cc8072d3053719
Eric <li114228@126.com>
2019-07-03 20:53:11 +0800

    修复编译错误

--- src/test/java/com/example/demo/controller/JvmTest.java
+++ src/test/java/com/example/demo/controller/JvmTest.java
@@ -7,17 +7,17 @@ public class JvmTest {
 
     }
 
-    /**
-     * 内存溢出测试
-     * 启动参数设置为：-Xms48m -Xmx48m -XX:+PrintGCDetails -XX:+UseParallelOldGC
-     */
-    public static void MakeOutOfMemery() {
-        String str = "";
-        for (int n = 0; n < Integer.MAX_VALUE; n++) {
-            str += n + str;
-            str.intern();
-        }
-    }
+//    /**
+//     * 内存溢出测试
+//     * 启动参数设置为：-Xms48m -Xmx48m -XX:+PrintGCDetails -XX:+UseParallelOldGC
+//     */
+//    public static void MakeOutOfMemery() {
+//        String str = "";
+//        for (int n = 0; n < Integer.MAX_VALUE; n++) {
+//            str += n + str;
+//            str.intern();
+//        }
+//    }
 
 
 }
