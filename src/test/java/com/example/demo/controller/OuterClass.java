5f4fbef9ca3f0c9161b475137b3dea432bb52b18
Eric <cyneck@qq.com>
2020-08-29 01:08:15 +0800

    feat:InnerClass

--- src/test/java/com/example/demo/controller/OuterClass.java
+++ src/test/java/com/example/demo/controller/OuterClass.java
@@ -8,8 +8,11 @@ package com.example.demo.controller;
  * @since 2020/8/28 18:12
  **/
 public class OuterClass {
-//    private static float f = 1.0f;
-    private  float f = 1.0f;
+    //    private static float f = 1.0f;
+    private float f = 1.0f;
+
+    public String name = "test";
+    private static int age = 20;
 
 //    class InnerClass {
 //        public static float func() {
@@ -33,6 +36,21 @@ public class OuterClass {
 //            return f;
 //        }
 //    }
+
+    /**
+     * 静态内部类和非静态内部类之间存在一个最大的区别，非静态内部类在编译完成之后会隐含的保存着一个引用
+     * 静态内部类的创建不需要依赖外部类可以直接创建。可以 new OuterClass.Inner()
+     */
+    static class Inner {
+
+        private String name;
+
+        public void fun() {
+            //静态内部类不可以使用任何外部类的非static类（包括属性和方法），但可以存在自己的成员变量
+            System.out.println(name);
+            System.out.println(age);
+        }
+    }
 
 
 }
