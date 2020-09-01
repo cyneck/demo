b25dbd946e7d012e68d4d4ac3ca9f1edac977de9
Eric Lee <cyneck@qq.com>
2020-09-01 17:16:55 +0800

    fixed

--- src/test/java/com/example/demo/controller/CLassLoaderTest.java
+++ src/test/java/com/example/demo/controller/CLassLoaderTest.java
@@ -26,8 +26,9 @@ public class CLassLoaderTest {
     private static final ArrayList<String> list = new ArrayList<>();
 
     public static String test(String j) {
-        int i = 1, s = 1, f = 1, a = 1, b = 1, c = 1, d = 1, e = 1;
-        list.add(new String("11111111111111111111111111111"));
-        return test(s + i + f + a + b + c + d + e + "");
+//        int i = 1, s = 1, f = 1, a = 1, b = 1, c = 1, d = 1, e = 1;
+//        list.add(new String("11111111111111111111111111111"));
+//        return test(s + i + f + a + b + c + d + e + "");
+        return "";
     }
 }
