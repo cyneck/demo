3aa4709c0e8618898a58541add4201a0977a141d
eric <cyneck@qq.com>
2021-02-19 23:12:46 +0800

    permutaion

--- src/main/java/com/example/demo/algorithms/LexicograhicPermutationSolution.java
+++ src/main/java/com/example/demo/algorithms/LexicograhicPermutationSolution.java
@@ -16,7 +16,7 @@ public class LexicograhicPermutationSolution {
 //        int[] list = {1, 2, 3};
 //        Prim(list);
 
-        int[] A = {1, 5, 2, 3,4};
+        int[] A = {3, 1, 4, 2};
         int[] R = nextPermutation(A);
     }
 
@@ -26,38 +26,45 @@ public class LexicograhicPermutationSolution {
         int n = A.length;
         int a = 0, b = 0;
         for (int i = n - 1; i > 0; --i) //从右往左，找出第一个左边小于右边的数，设为list[a]
+        {
             if (A[i - 1] < A[i]) {
                 a = i - 1;
                 break;
             }
+        }
+        if (a == 0) {
+            return null;
+        }
         for (int j = n - 1; j > a; --j) //从右往左，找出第一个大于list[a]的数，设为list[b]
+        {
             if (A[j] > A[a]) {
                 b = j;
                 break;
             }
-        print(A);
+        }
+        print("原始", A);
         Swap(A, a, b);         //交换list[a],list[b]
-        print(A);
-        a++;
+        print("交换ab", A);
 
         ArrayList arraylist = new ArrayList();
         for (int i = 0; i < A.length; i++) {
             arraylist.add(A[i]); //存放元素
         }
 
-        reverse(A, a, n - 1);
-        print(A);
-        List  list=arraylist.subList(a,n-1);
-        Collections.reverse(list); //使用方法进行逆序,验证
-        print(list);
+        reverse(A, a + 1, n - 1);
+        print("倒置剩余", A);
+//        List list = arraylist.subList(a, n - 1);
+//        Collections.reverse(list); //使用方法进行逆序,验证
+//        print(list);
         return A;
     }
 
     // 剩余数组倒转
     public static void reverse(int[] A, int start, int end) {
         while (start < end) {
-            start++;
             Swap(A, start, end);
+            start++;
+            end--;
         }
     }
 
@@ -71,7 +78,7 @@ public class LexicograhicPermutationSolution {
             num *= i;   //阶乘
         while (num-- != 1) {
             //打印情况
-            print(list);
+            print("开始", list);
             for (int i = n - 1; i > 0; --i) //从右往左，找出第一个左边小于右边的数，设为list[a]
                 if (list[i - 1] < list[i]) {
                     a = i - 1;
@@ -86,7 +93,7 @@ public class LexicograhicPermutationSolution {
             Sort(list, a, n);         //将list[a]后面的数据，由小往大排列
         }
         //打印情况
-        print(list);
+        print("结束", list);
         return;
     }
 
@@ -104,8 +111,8 @@ public class LexicograhicPermutationSolution {
         return;
     }
 
-    static void print(int[] list) {
-        String result = "";
+    static void print(String msg, int[] list) {
+        String result = msg + ": ";
         for (int item : list) {
             result += item;
 
@@ -113,8 +120,8 @@ public class LexicograhicPermutationSolution {
         System.out.println(result);
     }
 
-    static void print(List<Object> list) {
-        String result = "";
+    static void print(String msg, List<Object> list) {
+        String result = msg + ": ";
         for (Object item : list) {
             result += item;
 
